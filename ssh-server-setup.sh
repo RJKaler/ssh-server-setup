2ce30e01994c4fe17e475ed40cc0ed6cae5c9828
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2025-05-29 08:22:14 -0400

    Update ssh-server-setup.sh

--- ssh-server-setup.sh
+++ ssh-server-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 #Install basic open-ssh server package
 sudo apt-get install openssh-server -y || { echo "error" && exit 1; }
@@ -7,7 +7,7 @@ echo "generating keys now ..."
 #Basic for loop to auto-agree to the defaults of ssh-keygen
 for item in ssh-keygen
 do
-  yes "" | "$item" #this is where the magic starts with the auto-agree || { echo "error" && exit 1; } 
+yes "" | "$item" || { echo "error" && exit 1; }  #this is where the magic starts with the auto-agree  
 done
 
 echo "Success!"
